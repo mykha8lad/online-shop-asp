553d9d6fdb4ec09579a1d7c1b7250defa8947f27
Vladyslav Mykhailichenko <mykhailichenkovlad@gmail.com>
2024-12-07 01:54:58 +0200

    update

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -1,21 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Shop.Data;
 
 namespace Shop
 {
     public class Program
-    {   
+    {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Регистрируем DbContext с подключением к PostgreSQL
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
             builder.Services.AddControllersWithViews();
-            builder.WebHost.UseUrls("http://localhost:5000");
 
             var app = builder.Build();
 
@@ -29,13 +23,13 @@ namespace Shop
             app.UseStaticFiles();
 
             app.UseRouting();
+
             app.UseAuthorization();
 
             app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
-
 
 
     }
